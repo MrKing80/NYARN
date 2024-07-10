710c2606e3cb4e513efca03b78735a51b465ee78
MrKing80 <mr.king80729@gmail.com>
2024-07-10 16:47:44 +0900

    バックアップ持ってきた

---
+++ Assets/sasaki/Scripts/CameraMove.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    [SerializeField] private GameObject player = default;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerPos = player.transform.position;  //ポジション取得
+
+        this.transform.position = new Vector3(playerPos.x,playerPos.y,this.transform.position.z);   //カメラのポジションを変更
+    }
+}
