f94d02c13ecbf868b083d15a1f822adb86f3ec9f
Rlyeh-ruruie <20237063-tamaiakane@hcs.ac.jp>
2024-07-11 09:47:10 +0900

    1

---
+++ Assets/sasaki/Scripts/PlayerItemCatch.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerItemCatch : MonoBehaviour
+{
+    [SerializeField] private ItemDataBase.ItemData itemData;
+
+    private GameObject item = default;      //アイテムを格納する変数
+
+    private bool isItemTouch = false;       //アイテムが拾えるかどうか
+    private bool isDoNotThrow = false;      //アイテムが捨てられるかどうか
+    
+    private int i = 0;      //リストの添え字
+
+    private int zero = 0;   //0
+
+    [SerializeField] private List<GameObject> items = new List<GameObject>();    //アイテムのリスト
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        //アイテム取得
+        if(isItemTouch && Input.GetKeyDown("joystick button 0") || !isDoNotThrow && Input.GetMouseButtonDown(0))
+        {
+            items.Add(item);                //リスト追加
+            items[i].SetActive(false);      //アイテムを非表示
+            i++;                            //インクリメント
+            print("とった！");
+        }
+
+        //アイテム捨てる
+        if (!isDoNotThrow && Input.GetKeyDown("joystick button 1") || !isDoNotThrow && Input.GetMouseButtonDown(1))
+        {
+            if (items[zero] != null)
+            {
+                items[zero].SetActive(true);        //アイテム表示
+                items[zero].transform.position = this.transform.position;   //自分の足元へ落とす
+                items.Remove(items[zero]);          //リストから削除
+                i--;                                //デクリメント
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Item"))
+        {
+            item = collision.gameObject;    //触れたアイテムの情報取得
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Item"))
+        {
+            isItemTouch = true;         //触れている状態にする
+            isDoNotThrow = true;        //アイテムを落とせないようにする
+            print("アイテム！");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Item"))
+        {
+            isItemTouch = false;        //触れていない状態にする
+            isDoNotThrow = false;       //アイテムを落とせないようにする
+            print("アイテム....");
+        }
+    }
+}
---
+++ Assets/sasaki/Scripts/PlayerMove.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] private float speed = default;     //プレイヤーのスピード
+
+    private Rigidbody2D rig = default;                  //Rigidbody2Dを保存する変数
+    private Vector3 wallPos = default;                  
+    private float inputX = 0f;      //横方向のインプットされた値を保持する変数
+    private float inputY = 0f;      //縦方向のインプットされた値を保持する変数
+
+    private bool isWallTouch = false;   //壁に触れているか
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputX = Input.GetAxisRaw("Horizontal") * speed;    //プレイヤーの横方向の移動速度を格納
+        inputY = Input.GetAxisRaw("Vertical") * speed;      //プレイヤーの縦方向の移動速度を格納
+
+        //rig.velocity = new Vector2(inputX, inputY) * Time.deltaTime;
+        #region//プレイヤーのオブジェクトが手に入ったら調整必須
+        
+        //下の処理は移動に応じて角度が変わる処理　※ここから
+
+        if (inputX > 0)
+        {
+            this.transform.rotation = Quaternion.Euler(0f, 0f, -90f);
+        }
+        else if (inputX < 0)
+        {
+            this.transform.rotation = Quaternion.Euler(0f, 0f, 90f);
+        }
+
+        if (inputY > 0)
+        {
+            this.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+        }
+        else if (inputY < 0)
+        {
+            this.transform.rotation = Quaternion.Euler(0f, 0f, 180f);
+        }
+
+        //※ここまで
+        #endregion
+
+        if (!isWallTouch)
+        {
+            this.transform.position += (new Vector3(inputX, inputY) * Time.deltaTime);  //壁に触れていなければ移動
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            wallPos = collision.transform.position;
+            isWallTouch = true;
+        }
+
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Wall"))
+        {
+            isWallTouch = false;
+        }
+    }
+}
