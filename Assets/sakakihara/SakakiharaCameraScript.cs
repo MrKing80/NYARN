2e6e0d8aec23204d1df3913308c4d6eabdfeff83
Nana-777-7 <20237045-sakakiharananaka@hcs.ac.jp>
2024-07-16 15:33:31 +0900

    マップ作製

---
+++ Assets/sakakihara/SakakiharaCameraScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SakakiharaCameraScript : MonoBehaviour
+{
+    //仮でつけたカメラ移動スクリプト
+
+    [SerializeField] GameObject player;
+
+    void Update()
+    {
+        Vector3 playerPos = this.player.transform.position;
+
+            transform.position = new Vector3(
+                playerPos.x, playerPos.y, transform.position.z);//プレイヤーのについてくぞ☆
+
+    }
+}
