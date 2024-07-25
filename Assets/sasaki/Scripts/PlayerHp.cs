e346b8a5fb51537995d98f42327413537bc077f6
MrKing80 <mr.king80729@gmail.com>
2024-07-25 09:45:48 +0900

    無敵追加

--- Assets/sasaki/Scripts/PlayerHp.cs
+++ Assets/sasaki/Scripts/PlayerHp.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerHP : MonoBehaviour
 {
     [SerializeField] private int playerHp;
+    private bool isInvincible = false;
+    private float flgChangeTime = 0.5f;
+    private float countTime = 0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +17,18 @@ public class PlayerHP : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        print(isInvincible);
+        if (isInvincible)
+        {
+            countTime += Time.deltaTime;
+
+            if(countTime >= flgChangeTime)
+            {
+                countTime = 0;
+                isInvincible = false;
+            }
+        }
+
         if (playerHp <= 0)
         {
             print("gameover");
@@ -24,8 +39,16 @@ public class PlayerHP : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
-            playerHp--;
-            print("HIT");
+            if (isInvincible)
+            {
+                return;
+            }
+            else if (!isInvincible)
+            {
+                playerHp--;
+                print("HIT");
+                isInvincible = true;
+            }
         }
     }
 }
