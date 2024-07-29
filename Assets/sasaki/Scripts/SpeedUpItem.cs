96096db36917c7766b4d7631215ef6c78cdbf73a
MrKing80 <mr.king80729@gmail.com>
2024-07-29 12:26:03 +0900

    コメント書いた

--- Assets/sasaki/Scripts/SpeedUpItem.cs
+++ Assets/sasaki/Scripts/SpeedUpItem.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class SpeedUpItem : MonoBehaviour
 {
+    [SerializeField,Header("スピード倍率を入れるところだよー")] private float magnification = 3.9f;
+    
+    //プレイヤーの移動スクリプトを格納するところ
     private PlayerMove move = default;
+
+    //プレイヤーを格納するところ
     private GameObject player = default;
+
+    //バフの効果時間
     private float buffTime = 3f;
-    [SerializeField] private float magnification = 3.9f;
+
+    //一時的にスピードを格納するところ
     private float tmpSpeed = 0;
+
+    //プレイヤーの初期スピードを格納するところ
     private float initialSpeed = default;
+
+    //触れているか
     private bool isTouch = false;
+ 
+    //ゲットしているか
     private bool isGet = false;
     // Start is called before the first frame update
     void Start()
@@ -26,34 +40,36 @@ public class SpeedUpItem : MonoBehaviour
             return;
         }
 
+        //Aボタンもしくは左クリックを押したとき
         if (isTouch && (Input.GetKeyDown("joystick button 0") || Input.GetKeyDown(KeyCode.Mouse0)))
         {
-            move = player.GetComponent<PlayerMove>();
-            initialSpeed = move.SpeedProperty;
-            tmpSpeed = move.SpeedProperty * magnification;
-            print(tmpSpeed);
-            move.SpeedProperty = tmpSpeed;
-            this.GetComponent<SpriteRenderer>().enabled = false;
+            move = player.GetComponent<PlayerMove>();   //スクリプト取得
+         
+            initialSpeed = move.SpeedProperty;          //初期スピードを保存
+            tmpSpeed = move.SpeedProperty * magnification;  //スピードアップ
+            
+            move.SpeedProperty = tmpSpeed;      //スピード変更
+            
+            this.GetComponent<SpriteRenderer>().enabled = false;    
+                                                                        //非表示            
             this.GetComponent<CircleCollider2D>().enabled = false;
+            
             isGet = true;
         }
-        
-        if(isGet)
-        {
-            buffTime -= Time.deltaTime;
-            print(buffTime);
 
-            if(buffTime <= 0)
+        if (isGet)
+        {
+            buffTime -= Time.deltaTime;     //時間計測
+
+            //効果時間終了したら
+            if (buffTime <= 0)
             {
-                move.SpeedProperty = initialSpeed;
-                player = null;
-                isGet = false;
+                move.SpeedProperty = initialSpeed;  //スピードを元に戻す
+                player = null;      
+                isGet = false;      //各種変数初期化
                 buffTime = 3f;
             }
         }
-
-        print(isGet);
-
     }
 
     private void OnTriggerStay2D(Collider2D collision)
@@ -67,7 +83,7 @@ public class SpeedUpItem : MonoBehaviour
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if(collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player"))
         {
             isTouch = false;
         }
