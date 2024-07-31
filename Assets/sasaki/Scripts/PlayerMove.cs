4c54c0300041f37436b4fd3a925f2c10d5f5e74c
MrKing80 <mr.king80729@gmail.com>
2024-07-31 15:23:44 +0900

    アニメーション実装
    向きの固定が未実装

--- Assets/sasaki/Scripts/PlayerMove.cs
+++ Assets/sasaki/Scripts/PlayerMove.cs
@@ -11,11 +11,28 @@ public class PlayerMove : MonoBehaviour
 
     private Rigidbody2D rig = default;                  //Rigidbody2Dを保存する変数
 
+    private Animator playerAnimator = default;
+
     private float inputX = 0f;      //横方向のインプットされた値を保持する変数
     private float inputY = 0f;      //縦方向のインプットされた値を保持する変数
 
     private bool isWallTouch = false;   //壁に触れているか
 
+    private enum PLAYER_STATUS
+    {
+        FORWARD,
+        FORWARDWALK,
+        LEFT,
+        LEFTWALK,
+        RIGHT,
+        RIGHTWALK,
+        BEHIND,
+        BEHINDWALK,
+    }
+
+    /// <summary>
+    /// スピードを受け渡すプロパティ
+    /// </summary>
     public float SpeedProperty
     {
         get { return speed; }
@@ -25,13 +42,14 @@ public class PlayerMove : MonoBehaviour
     void Start()
     {
         rig = this.GetComponent<Rigidbody2D>();
+        playerAnimator = this.GetComponent<Animator>();
         //itemCatch = this.GetComponent<PlayerItemCatch>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Time.timeScale == 0)
+        if (Time.timeScale == 0)
         {
             return;
         }
@@ -43,29 +61,151 @@ public class PlayerMove : MonoBehaviour
 
         rig.velocity = new Vector2(inputX, inputY);
 
-        #region//プレイヤーのオブジェクトが手に入ったら調整必須
+        MoveRotation();
+    }
 
-        //下の処理は移動に応じて角度が変わる処理　※ここから
+    private void MoveRotation()
+    {
+        PLAYER_STATUS state = default;
 
         if (inputX > 0)
         {
-            this.transform.rotation = Quaternion.Euler(0f, 0f, -90f);
+            state = PLAYER_STATUS.RIGHTWALK;
         }
         else if (inputX < 0)
         {
-            this.transform.rotation = Quaternion.Euler(0f, 0f, 90f);
+            state = PLAYER_STATUS.LEFTWALK;
         }
 
         if (inputY > 0)
         {
-            this.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+            state = PLAYER_STATUS.FORWARDWALK;
         }
         else if (inputY < 0)
         {
-            this.transform.rotation = Quaternion.Euler(0f, 0f, 180f);
+            state = PLAYER_STATUS.BEHINDWALK;
         }
 
-        //※ここまで
-        #endregion
+        if (inputX == 0 && state == PLAYER_STATUS.LEFTWALK)
+        {
+            state = PLAYER_STATUS.LEFT;
+        }
+        else if (inputX == 0 && state == PLAYER_STATUS.RIGHTWALK)
+        {
+            state = PLAYER_STATUS.RIGHT;
+        }
+
+        if (inputY == 0 && state == PLAYER_STATUS.FORWARDWALK)
+        {
+            state = PLAYER_STATUS.FORWARD;
+        }
+        else if (inputY == 0 && state == PLAYER_STATUS.BEHINDWALK)
+        {
+            state = PLAYER_STATUS.BEHIND;
+        }
+
+        switch (state)
+        {
+            case PLAYER_STATUS.FORWARD:
+                playerAnimator.SetBool("forward", true);
+                playerAnimator.SetBool("forwardwalk", false);
+                playerAnimator.SetBool("left", false);
+                playerAnimator.SetBool("leftwalk", false);
+                playerAnimator.SetBool("right", false);
+                playerAnimator.SetBool("rightwalk", false);
+                playerAnimator.SetBool("behind", false);
+                playerAnimator.SetBool("behindwalk", false);
+                break;
+
+            case PLAYER_STATUS.FORWARDWALK:
+                playerAnimator.SetBool("forward", false);
+                playerAnimator.SetBool("forwardwalk", true);
+                playerAnimator.SetBool("left", false);
+                playerAnimator.SetBool("leftwalk", false);
+                playerAnimator.SetBool("right", false);
+                playerAnimator.SetBool("rightwalk", false);
+                playerAnimator.SetBool("behind", false);
+                playerAnimator.SetBool("behindwalk", false);
+                break;
+
+            case PLAYER_STATUS.LEFT:
+                playerAnimator.SetBool("forward", false);
+                playerAnimator.SetBool("forwardwalk", false);
+                playerAnimator.SetBool("left", true);
+                playerAnimator.SetBool("leftwalk", false);
+                playerAnimator.SetBool("right", false);
+                playerAnimator.SetBool("rightwalk", false);
+                playerAnimator.SetBool("behind", false);
+                playerAnimator.SetBool("behindwalk", false);
+                break;
+            
+            case PLAYER_STATUS.LEFTWALK:
+                playerAnimator.SetBool("forward", false);
+                playerAnimator.SetBool("forwardwalk", false);
+                playerAnimator.SetBool("left", false);
+                playerAnimator.SetBool("leftwalk", true);
+                playerAnimator.SetBool("right", false);
+                playerAnimator.SetBool("rightwalk", false);
+                playerAnimator.SetBool("behind", false);
+                playerAnimator.SetBool("behindwalk", false);
+                break;
+            
+            case PLAYER_STATUS.RIGHT:
+                playerAnimator.SetBool("forward", false);
+                playerAnimator.SetBool("forwardwalk", false);
+                playerAnimator.SetBool("left", false);
+                playerAnimator.SetBool("leftwalk", false);
+                playerAnimator.SetBool("right", true);
+                playerAnimator.SetBool("rightwalk", false);
+                playerAnimator.SetBool("behind", false);
+                playerAnimator.SetBool("behindwalk", false);
+                break;
+            
+            case PLAYER_STATUS.RIGHTWALK:
+                playerAnimator.SetBool("forward", false);
+                playerAnimator.SetBool("forwardwalk", false);
+                playerAnimator.SetBool("left", false);
+                playerAnimator.SetBool("leftwalk", false);
+                playerAnimator.SetBool("right", false);
+                playerAnimator.SetBool("rightwalk", true);
+                playerAnimator.SetBool("behind", false);
+                playerAnimator.SetBool("behindwalk", false);
+                break;
+            
+            case PLAYER_STATUS.BEHIND:
+                playerAnimator.SetBool("forward", false);
+                playerAnimator.SetBool("forwardwalk", false);
+                playerAnimator.SetBool("left", false);
+                playerAnimator.SetBool("leftwalk", false);
+                playerAnimator.SetBool("right", false);
+                playerAnimator.SetBool("rightwalk", false);
+                playerAnimator.SetBool("behind", true);
+                playerAnimator.SetBool("behindwalk", false);
+                break;
+            
+            case PLAYER_STATUS.BEHINDWALK:
+                playerAnimator.SetBool("forward", false);
+                playerAnimator.SetBool("forwardwalk", false);
+                playerAnimator.SetBool("left", false);
+                playerAnimator.SetBool("leftwalk", false);
+                playerAnimator.SetBool("right", false);
+                playerAnimator.SetBool("rightwalk", false);
+                playerAnimator.SetBool("behind", false);
+                playerAnimator.SetBool("behindwalk", true);
+                break;
+
+            default:
+                playerAnimator.SetBool("forward", false);
+                playerAnimator.SetBool("forwardwalk", false);
+                playerAnimator.SetBool("left", false);
+                playerAnimator.SetBool("leftwalk", false);
+                playerAnimator.SetBool("right", false);
+                playerAnimator.SetBool("rightwalk", false);
+                playerAnimator.SetBool("behind", false);
+                playerAnimator.SetBool("behindwalk", false);
+                break;
+        }
+
+        print(state);
     }
 }
