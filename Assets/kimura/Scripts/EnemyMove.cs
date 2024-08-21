4f4ddf235576b98ae19981c050216b29f630ecd6
MrKing80 <mr.king80729@gmail.com>
2024-08-21 14:19:47 +0900

    enemy導入

--- Assets/kimura/Scripts/EnemyMove.cs
+++ Assets/kimura/Scripts/EnemyMove.cs
@@ -10,11 +10,22 @@ public class EnemyMove : MonoBehaviour
 
     EnemyVisionScript GetVison;
     NavMeshAgent2D Agent2D;
-    public Transform MyTrans;
-    public Vector2 MyVector;
+    Animator EnemyAnimator;
+    private Transform MyTrans;
+    public Transform GetMyTrans//MyTransのプロパティ
+    {
+        get { return MyTrans; }
+        set { MyTrans = value; }
+    }
+    private Vector2 MyVector;
     private float _initialDistance;
     [Header("自分の初期位置")]
-    public Vector2 InitialPosition;//自分の初期位置  
+    private Vector2 InitialPosition;//自分の初期位置  
+    public Vector2 GetInitialPos//InitialPositioのプロパティ
+    {
+        get { return InitialPosition; }
+        set { InitialPosition = value; }
+    }
     [Header("自分のスピード")]
     [SerializeField] private float _moveSpeed = 4f;
     
@@ -23,32 +34,63 @@ public class EnemyMove : MonoBehaviour
     {
         //GetTracking = this.GetComponent<EnemyTracking>();
         MyTrans = this.GetComponent<Transform>();//自分のTransformを取得
+        EnemyAnimator = this.GetComponent<Animator>();
         Agent2D=this.GetComponent<NavMeshAgent2D>();
         InitialPosition = MyTrans.position;//自分の初期位置を取得
+        GetVison = GetComponent<EnemyVisionScript>();//子オブジェクトからEnemyVisionスクリプトを取得
         SetDirection();
-        GetVison = GetComponentInChildren<EnemyVisionScript>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         _initialDistance = Vector2.Distance(InitialPosition, MyVector);
-        MyVector = MyTrans.position;//自分の向きを取得 
-        if (GetVison.isPatrol)
+        MyVector = MyTrans.position;//自分の向きを取得
+        if (GetVison.existIsPatrol)//警備中だったら
         {
-            MyTrans.Translate(GetVison.VisionVec * Time.deltaTime);//巡回させる
-        }
-       
-        //if (GetVison.isPatrol && _initialDistance >= 5)
-        //{
-        //    GetVison.VisionVec = (InitialPosition - MyVector).normalized;
-        //    print("巡回"); 
-        //}
+            MyTrans.Translate(GetVison.GetVisonVec * Time.deltaTime);//巡回させる
+            switch (GetVison.GetMyRotation)
+            {
 
-        //else if(GetVison.isPatrol&&_initialDistance <= 0.01f)
-        //{
-        //    //GetVison.VisionVec =
-        //}
+                case 90:
+                    EnemyAnimator.Play("Behindwalk");
+                    break;
+                case 270:
+                    EnemyAnimator.Play("forwardwalk");
+                    break;
+                case 0:
+                    EnemyAnimator.Play("rightwalk");
+                    break;
+                case 180:
+                    EnemyAnimator.Play("leftwalk");
+                    break;
+
+            }
+        }
+
+        else
+        {
+            switch (GetVison.GetMyRotation)
+            {
+
+                case 90:
+                    EnemyAnimator.Play("Behind");
+                    break;
+                case 270:
+                    EnemyAnimator.Play("forward");
+                    break;
+                case 0:
+                    EnemyAnimator.Play("right");
+                    break;
+                case 180:
+                    EnemyAnimator.Play("left");
+                    break;
+            }
+        }
+
+       
+      
     }
     void SetDirection()
     {
--- Assets/kimura/Scripts/EnemyVisionScript.cs
+++ Assets/kimura/Scripts/EnemyVisionScript.cs
@@ -5,44 +5,91 @@ using UnityEngine;
 
 public class EnemyVisionScript : MonoBehaviour
 {
-    public enum MoveDirection
+    public enum MoveDirection//移動方向を決めるときの定数
     {
         Up,
         Down,
         Left,
         Right
     }
-    public enum ChangeAngle
+    public enum ChangeAngle//障害物に衝突した際どれくらい角度を変えるか決めるときの定数
     {
-        One = 90,
-        Two = 180,
-        Tree = 270
+        Angle90 = 90,
+        Angle180 = 180,
+        Angle270 = 270
     }
     [Header("巡回時の移動方向")]
-    [SerializeField] private MoveDirection _direction = MoveDirection.Right;
-    [Header("巡回時の角度調節")]
-    [SerializeField] private ChangeAngle GetAngle = ChangeAngle.One;//デフォルトは９０度回転させる
+    [SerializeField] private MoveDirection _direction = MoveDirection.Right;//デフォルトは右向き
+    [Header("衝突時の角度調節")]
+    [SerializeField] private ChangeAngle GetAngle = ChangeAngle.Angle90;//デフォルトは９０度回転させる
     [Header("障害物のレイヤー")]
     [SerializeField] private LayerMask ObstacleLayer;
+    [Header("プレイヤーのレイヤー")]
+    [SerializeField] private LayerMask TargetLayer;
     [Header("レイの距離  ")]
     [SerializeField] private float _rayDistance = 5f;
-     private GameObject ParentObject;//親オブジェクトを格納する場所
-    public Vector2 VisionVec;//視線の向き
-    public float _myRotation;//視線の角度
-    public Transform VisionTrans;//自分の位置
+    [Header("レイの半径")]
+    [SerializeField] private float _rayRadius = 1f;
+    [SerializeField] private float _maxDistance = 10f;
+    [Header("立ち止まる時間")]
+    [SerializeField] private float _stopTime = 5f;
+    [SerializeField] private  float _initialValue = 5f;//汎用の初期値（テスト用）
+    public Vector2 GetVisonVec//VisionVecのプロパティ
+    {
+        get { return VisionVec; }
+        set { VisionVec = value; }
+    }
+    private Transform VisionTrans;//自分の位置
+    [Header("停止するか制御する")]
+    [SerializeField] private bool isStop = false;//停止させるためのフラグ
     [Header("巡回させるか制御する")]
-    public bool isPatrol = true;//パトロール中かどうか制御する
-    private float _radians;//角度を向きに変換するための数値
-    
+    private bool isPatrol = true;//パトロール中かどうか制御する
+    public bool existIsPatrol//isPatrolのプロパティ
+    {
+        get { return isPatrol; }
+        set { isPatrol = value; }
+    }
+    private float _myRotation;//視線の角度 
+    public float GetMyRotation//_myRotationのプロパティ
+    {
+        get { return _myRotation; }
+        set { _myRotation = value; }
+    }
+    private float _radians;//角度を向きに変換するための変数
+    private float _angleOffset = 15f;//hit2,hit3の角度調節に使う変数
+    private float _currentRotation = default;//myRotationの値を取得するための変数
+    private float _turnAroundTime = 0.5f;
+    private float _turnAroundTimeValue = 0.5f;
+    private float _presenceAngle;//衝突時に振り向かせるための変数
+    private Vector2 VisionVec;//視線の向き
+    private Vector2 Hit2Vec;
+    private Vector2 Hit3Vec;
     RaycastHit2D ObstacleRay;//障害物を見分ける視線
+    RaycastHit2D PresenceRay;//死角でもプレイヤーを察知できるようにする球状のレイ
+    RaycastHit2D hit1;
+    RaycastHit2D hit2;
+    RaycastHit2D hit3;
+    public RaycastHit2D GetHit1
+    {
+        get { return hit1;}
+    }
+    public RaycastHit2D GetHit2
+    {
+        get { return hit2; }
+    }
+
+    public RaycastHit2D GetHit3
+    {
+        get { return hit3; }
+    }
+    EnemyTracking GetTracking;
     // Start is called before the first frame update
     void Start()
-    {
-        ParentObject = transform.parent.gameObject;
-        print(ParentObject.name);
+    {               
         VisionTrans = this.GetComponent<Transform>();
-        _myRotation = VisionTrans.rotation.z;//視線の角度を取得
-        if (isPatrol)
+        GetTracking = GetComponent<EnemyTracking>();
+        _myRotation = VisionTrans.rotation.z;//角度を取得
+        if (isPatrol)//警備中だったらEnemyMoveスクリプトで移動させる
         {
             switch (_direction)//最初に移動する方向
             {
@@ -58,27 +105,96 @@ public class EnemyVisionScript : MonoBehaviour
                 case MoveDirection.Left:
                     _myRotation += 180;
                     break;
-              
             }
         }
+        _currentRotation = _myRotation;//myRotationの値を取得
     }
 
     // Update is called once per frame
     void Update()
     {
-        VisionControl();
-        ObstacleRay = Physics2D.Raycast(VisionTrans.position, VisionVec, _rayDistance, ObstacleLayer);
-        if (isPatrol && ObstacleRay.collider!=null && ObstacleRay.collider.gameObject!=ParentObject)//巡回時、障害物に当たったら
+        VisionConvert();
+        hit1 = Physics2D.Raycast(VisionTrans.position, VisionVec, _rayDistance, TargetLayer);//正面のレイ
+        hit2 = Physics2D.Raycast(VisionTrans.position, Hit2Vec, _rayDistance, TargetLayer);
+        hit3 = Physics2D.Raycast(VisionTrans.position, Hit3Vec, _rayDistance, TargetLayer);
+        Debug.DrawRay(VisionTrans.position, GetVisonVec * _rayDistance, Color.red);
+        Debug.DrawRay(VisionTrans.position, Hit2Vec * _rayDistance, Color.blue);
+        Debug.DrawRay(VisionTrans.position, Hit3Vec * _rayDistance, Color.green);
+        ObstacleRay = Physics2D.Raycast(VisionTrans.position, VisionVec, _rayDistance, ObstacleLayer);//障害物を見分けるレイ
+        PresenceRay = Physics2D.CircleCast(VisionTrans.position, _rayRadius, VisionVec, _maxDistance, TargetLayer);//死角でもプレイヤー察知できるようにするレイ
+
+        if (isPatrol && ObstacleRay.collider != null && ObstacleRay.collider.gameObject != this.gameObject)//巡回時、障害物に当たったら
         {
             print("あたった");
-            _myRotation += (int)GetAngle;//視線をGetAngleで指定した角度に傾かせる
+            _myRotation += (int)GetAngle;//視線をGetAngleで指定した角度に傾かせる 　
+            if (_myRotation >= 360)//360度超えたら角度リセット
+            {
+                _myRotation -= 360;
+                _currentRotation = _myRotation;//再度myRotationの値を取得
+                print(_currentRotation);
+            }
+            print(_myRotation);
         }
 
+        if (PresenceRay)//索敵範囲にプレイヤーが衝突したら
+        {
+            print("なんや?");
+            print(PresenceRay.collider.gameObject.name);//テスト用に名前を取得する
+            isPatrol = false;//巡回をやめる
+            isStop = true;//立ち止まる
+            _stopTime = _initialValue;
+            _turnAroundTime = _turnAroundTimeValue;
+            _presenceAngle = Mathf.Atan2(PresenceRay.point.y, PresenceRay.point.x) * Mathf.Rad2Deg;//衝突したオブジェクトの座標を取得
+            
+        }
+
+        if (isStop)//止まったら
+        {
+            _stopTime -= Time.deltaTime;//立ち止まる時間をカウントダウン
+            _turnAroundTime -= Time.deltaTime;//振り向くまでの時間をカウントダウン
+            if (_turnAroundTime <= 0)//一定時間立ち止まったら
+            {
+                TurnAngle();//プレイヤーがいた位置に振り向く
+                print("ふりむけ");
+            }
+            if (_stopTime <= 0)//０秒になったら（振り向く前にプレイヤーが移動したら)
+            {        
+                _myRotation = _currentRotation;//_myRotationを巡回していた時の角度にもどす
+                isPatrol = true;//再び巡回させる
+                isStop = false;
+                _stopTime = _initialValue;//初期値に戻す
+                _turnAroundTime = _turnAroundTimeValue;//いずれマジックナンバー消す
+            }
+        }
     }
 
-    void VisionControl()
+    /// <summary>
+    ///  角度を向きに変換するメソッド
+    /// </summary>
+    void VisionConvert()
     {
         _radians = _myRotation * Mathf.Deg2Rad;//視線の角度を向きに変換
         VisionVec = new Vector2(Mathf.Cos(_radians), Mathf.Sin(_radians));//_radiansから視線の向きを取得
+        float Hit2Angle = _myRotation + _angleOffset * Mathf.Deg2Rad;
+        Hit2Vec = new Vector2(Mathf.Cos(Hit2Angle), Mathf.Sin(Hit2Angle));
+        float Hit3Angle = _myRotation - _angleOffset * Mathf.Deg2Rad;
+        Hit3Vec = new Vector2(Mathf.Cos(Hit3Angle), Mathf.Sin(Hit3Angle));
     }
+
+    /// <summary>
+    ///  指定した角度に振り向かせるメソッド
+    /// </summary>
+    void TurnAngle()
+    {
+        //_presenceAngle = Mathf.Atan2(PresenceRay.point.y, PresenceRay.point.x) * Mathf.Rad2Deg;//衝突したオブジェクトの座標を取得
+        _myRotation = _presenceAngle;//PresenceAngleの値を取得してその値の向きに合わせる
+    }
+
+    void OnDrawGizmos()//PresenceRayを可視化
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireSphere(VisionTrans.position, _rayRadius);
+    }
+
+
 }
