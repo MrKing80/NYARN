a173c815f0047d8a355da12664558671c07802d2
MrKing80 <mr.king80729@gmail.com>
2024-08-29 13:37:29 +0900

    インベントリ操作完全実装
    アイテムを保持する部分を作り始める

--- Assets/sasaki/Scripts/S_InventoryManager.cs
+++ Assets/sasaki/Scripts/S_InventoryManager.cs
@@ -8,13 +8,18 @@ public class S_InventoryManager : MonoBehaviour
     [SerializeField] private GameObject toolbar = default;
     [SerializeField] private GameObject inventory = default;
     private GameObject[,] inventorySlots = new GameObject[3, 9];
+    private S_InventorySlotScript inventorySlotScript = default;
     private int k = 0;
+    private int v = 0;
+    private int h = 0;
     private bool isOpen = false;
+    private bool isHorizontalButtonPush = false;
+    private bool isVerticalButtonPush = false;
     private void Awake()
     {
-        for(int i = 0; i < inventorySlots.GetLength(0);i++)
+        for (int i = 0; i < inventorySlots.GetLength(0); i++)
         {
-            for(int j = 0; j < inventorySlots.GetLength(1);j++)
+            for (int j = 0; j < inventorySlots.GetLength(1); j++)
             {
                 inventorySlots[i, j] = inventory.transform.GetChild(k).gameObject;
                 print(inventorySlots[i, j]);
@@ -33,9 +38,93 @@ public class S_InventoryManager : MonoBehaviour
     {
         InventoryOpenAndClose();
 
-        if(isOpen)
+        if (isOpen)
         {
-            
+
+            if (Input.GetAxisRaw("D-padHorizontal") == 1 && !isHorizontalButtonPush)
+            {
+                isHorizontalButtonPush = true;
+
+                inventorySlotScript = inventorySlots[v, h].GetComponent<S_InventorySlotScript>();
+                inventorySlotScript.NotSelect();
+
+                if (h < inventorySlots.GetLength(1) - 1)
+                {
+                    h++;
+                }
+
+                inventorySlotScript = inventorySlots[v, h].GetComponent<S_InventorySlotScript>();
+                inventorySlotScript.Select();
+
+            }
+            else if (Input.GetAxisRaw("D-padHorizontal") == 0 && isHorizontalButtonPush)
+            {
+                isHorizontalButtonPush = false;
+            }
+
+            if (Input.GetAxisRaw("D-padHorizontal") == -1 && !isHorizontalButtonPush)
+            {
+                isHorizontalButtonPush = true;
+
+                inventorySlotScript = inventorySlots[v, h].GetComponent<S_InventorySlotScript>();
+                inventorySlotScript.NotSelect();
+
+                if (h > 0)
+                {
+                    h--;
+                }
+
+                inventorySlotScript = inventorySlots[v, h].GetComponent<S_InventorySlotScript>();
+                inventorySlotScript.Select();
+
+            }
+            else if (Input.GetAxisRaw("D-padHorizontal") == 0 && isHorizontalButtonPush)
+            {
+                isHorizontalButtonPush = false;
+            }
+
+            if (Input.GetAxisRaw("D-padVertical") == 1 && !isVerticalButtonPush)
+            {
+                isVerticalButtonPush = true;
+
+                inventorySlotScript = inventorySlots[v, h].GetComponent<S_InventorySlotScript>();
+                inventorySlotScript.NotSelect();
+
+                if (v > 0)
+                {
+                    v--;
+                }
+
+                inventorySlotScript = inventorySlots[v, h].GetComponent<S_InventorySlotScript>();
+                inventorySlotScript.Select();
+
+            }
+            else if (Input.GetAxisRaw("D-padVertical") == 0 && isVerticalButtonPush)
+            {
+                isVerticalButtonPush = false;
+            }
+
+            if (Input.GetAxisRaw("D-padVertical") == -1 && !isVerticalButtonPush)
+            {
+                isVerticalButtonPush = true;
+
+                inventorySlotScript = inventorySlots[v, h].GetComponent<S_InventorySlotScript>();
+                inventorySlotScript.NotSelect();
+
+                if (v < inventorySlots.GetLength(0) - 1)
+                {
+                    v++;
+                }
+
+                inventorySlotScript = inventorySlots[v, h].GetComponent<S_InventorySlotScript>();
+                inventorySlotScript.Select();
+
+            }
+            else if (Input.GetAxisRaw("D-padVertical") == 0 && isVerticalButtonPush)
+            {
+                isVerticalButtonPush = false;
+            }
+
         }
     }
 
@@ -52,6 +141,10 @@ public class S_InventoryManager : MonoBehaviour
             {
                 isOpen = true;
                 inventory.SetActive(true);
+
+                inventorySlotScript = inventorySlots[v, h].GetComponent<S_InventorySlotScript>();
+                inventorySlotScript.Select();
+
             }
         }
 
