508235ec6dfdaefd87ce249cd31be1637c3130fe
Nana-777-7 <20237045-sakakiharananaka@hcs.ac.jp>
2024-09-18 18:06:42 +0900

    保存

---
+++ Assets/sakakihara/Script/KeyButtonScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyButtonScript : MonoBehaviour
+{
+    [SerializeField] Button reviveYesButton;
+    [SerializeField] Button reviveNoButton;
+
+    private void Start()
+    {
+        
+    }
+}
