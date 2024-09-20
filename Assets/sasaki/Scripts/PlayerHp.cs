0e47b44ad6137b1587921dddd3a8f4f362302a91
MrKing80 <mr.king80729@gmail.com>
2024-09-20 13:30:48 +0900

    ゲームおーば関連を調整

--- Assets/sasaki/Scripts/PlayerHp.cs
+++ Assets/sasaki/Scripts/PlayerHp.cs
@@ -11,24 +11,16 @@ public class PlayerHP : MonoBehaviour
 
     [SerializeField, Header("ゲームオーバーテキスト入れるところだよ～")] private TMP_Text gameOverUi;
 
-    [SerializeField, Header("ゲームオーバーテキスト入れるところだよ～")] private GameObject continueUi;
-
     //無敵かどうか
     private bool isInvincible = false;
 
     //切り替え時間
     private float flgChangeTime = 1.5f;
 
-    private float intarval = 1.0f;
-
-    //カウント時間
-    private float countTime = 0f;
     // Start is called before the first frame update
     void Start()
     {
         gameOverUi.enabled = false;
-
-        continueUi.SetActive(false);
     }
 
     // Update is called once per frame
@@ -41,17 +33,6 @@ public class PlayerHP : MonoBehaviour
 
         if (gameOverUi.enabled)
         {
-            countTime += Time.deltaTime;
-
-            if(intarval <= countTime)
-            {
-                Time.timeScale = 0;
-
-                gameOverUi.enabled = false;
-                countTime = 0f;
-                continueUi.SetActive(true);
-
-            }
             playerHp = +maxHP;
         }
     }
