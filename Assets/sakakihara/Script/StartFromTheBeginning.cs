860f3d106c964e0c3207c7b66a3f5af2e987c185
Nana-777-7 <20237045-sakakiharananaka@hcs.ac.jp>
2024-09-20 16:18:37 +0900

    タイトルキーボード対応可

--- Assets/sakakihara/Script/StartFromTheBeginning.cs
+++ Assets/sakakihara/Script/StartFromTheBeginning.cs
@@ -9,11 +9,6 @@ public class StartFromTheBeginning : MonoBehaviour
     [SerializeField, Multiline(1)]//説明欄
     private string scriptMemo = ("ボタンを押したらすぐゲームが始まるスクリプト");
 
-    private void Awake()
-    {
-        SceneManager.LoadSceneAsync("Volume_AND_Tutorial", LoadSceneMode.Additive);
-    }
-
     public void FirstStageStarts()   //ボタン押したら
     {
         SceneManager.LoadScene("MainGameScene");//すぐにシーン遷移するぞ
