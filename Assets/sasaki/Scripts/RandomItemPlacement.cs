04bb69dde5c4b4299b3b2925f105725493fa171f
MrKing80 <mr.king80729@gmail.com>
2024-10-25 16:43:11 +0900

    ランダムにアイテムを設置できるようになったよ

---
+++ Assets/sasaki/Scripts/RandomItemPlacement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomItemPlacement : MonoBehaviour
+{
+    [SerializeField] private GameObject[] items = default;
+    private GameObject childObject = default;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int i = Random.Range(0, 6);
+        childObject = Instantiate(items[i], transform.position, Quaternion.identity);
+        childObject.transform.parent = transform.parent;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
